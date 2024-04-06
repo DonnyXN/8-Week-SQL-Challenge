f68ffdebe05f6e663eba71ce3919d5c2b299631b
Donny Nguyen <donnyngn@gmail.com>
2024-04-06 16:40:06 -0700

    Update Answers.sql
    Completed section A questions

--- Case Study #4/Answers.sql
+++ Case Study #4/Answers.sql
@@ -23,7 +23,7 @@ FROM data_bank.customer_nodes
 
 SELECT 
 	r.region_name AS region,
-	COUNT(DISTINCT(n.node_id)) AS nodes
+	COUNT(n.node_id) AS nodes
 FROM data_bank.regions r
 JOIN data_bank.customer_nodes n ON r.region_id = n.region_id
 GROUP BY r.region_name
@@ -32,7 +32,7 @@ GROUP BY r.region_name
 
 SELECT 
 	r.region_name AS region,
-	COUNT(*) AS customers
+	COUNT(DISTINCT(customer_id)) AS customers
 FROM data_bank.regions r
 JOIN data_bank.customer_nodes n ON r.region_id = n.region_id
 GROUP BY r.region_name
@@ -42,11 +42,12 @@ GROUP BY r.region_name
 WITH days_per_node AS (
 	SELECT
 		customer_id,
-		start_date,
-		end_date,
-		end_date - start_date AS days
+		node_id,
+		SUM(end_date - start_date) AS days
 	FROM data_bank.customer_nodes
 	WHERE DATE_PART('year', end_date) <> 9999
+	GROUP BY customer_id, 
+		node_id
 )
 
 SELECT 
@@ -55,7 +56,28 @@ FROM days_per_node
 
 -- 5. What is the median, 80th and 95th percentile for this same reallocation days metric for each region?
 
+WITH days_per_node AS (
+	SELECT
+		r.region_name AS region,
+		customer_id,
+		node_id,
+		SUM(end_date - start_date) AS days
+	FROM data_bank.customer_nodes n
+	JOIN data_bank.regions r ON r.region_id = n.region_id
+	WHERE DATE_PART('year', end_date) <> 9999
+	GROUP BY region,
+		customer_id, 
+		node_id
+)
 
+SELECT 
+	region,
+	ROUND(AVG(days), 1) AS avg_days,
+	ROUND(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY days ASC)::NUMERIC, 1) AS median,
+	ROUND(PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY days ASC)::NUMERIC, 1) AS percentile_80,
+	ROUND(PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY days ASC)::NUMERIC, 1) AS percentile_95
+FROM days_per_node
+GROUP BY region
 
 
 
