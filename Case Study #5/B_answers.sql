414abd89da760dd64fc37b032d424c1f29393830
Donny Nguyen <donnyngn@gmail.com>
2024-04-09 19:51:08 -0700

    Update B_answers.sql
    Numbered each questions

--- Case Study #5/B_answers.sql
+++ Case Study #5/B_answers.sql
@@ -4,13 +4,13 @@ FROM data_mart.clean_weekly_sales
 
 -- B. Data Exploration
 
--- What day of the week is used for each week_date value?
+-- 1. What day of the week is used for each week_date value?
 
 SELECT
 	DISTINCT(TO_CHAR(week_date, 'Day')) AS week_day
 FROM data_mart.clean_weekly_sales
 
--- What range of week numbers are missing from the dataset?
+-- 2. What range of week numbers are missing from the dataset?
 
 WITH week_numbers AS (
 SELECT
@@ -25,7 +25,7 @@ LEFT JOIN data_mart.clean_weekly_sales s ON s.week_number = n.numbers
 WHERE s.week_number IS null
 ORDER BY numbers
 
--- How many total transactions were there for each year in the dataset?
+-- 3. How many total transactions were there for each year in the dataset?
 
 SELECT
 	year_number,
@@ -33,12 +33,12 @@ SELECT
 FROM data_mart.clean_weekly_sales
 GROUP BY year_number
 
--- What is the total sales for each region for each month?
--- What is the total count of transactions for each platform
--- What is the percentage of sales for Retail vs Shopify for each month?
--- What is the percentage of sales by demographic for each year in the dataset?
--- Which age_band and demographic values contribute the most to Retail sales?
--- Can we use the avg_transaction column to find the average transaction size for each year for Retail vs Shopify? If not - how would you calculate it instead?
+-- 4. What is the total sales for each region for each month?
+-- 5. What is the total count of transactions for each platform
+-- 6. What is the percentage of sales for Retail vs Shopify for each month?
+-- 7. What is the percentage of sales by demographic for each year in the dataset?
+-- 8. Which age_band and demographic values contribute the most to Retail sales?
+-- 9. Can we use the avg_transaction column to find the average transaction size for each year for Retail vs Shopify? If not - how would you calculate it instead?
 
 
 
